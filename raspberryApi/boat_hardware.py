4c1d3b33b9a7a8fdd956f773305d44f789fd42e5
Sergey Bachev <bachev93@mail.ru>
2020-10-21 23:17:34 +0300

    настроил библиотеку RPi.GPIO для работы с raspberryPi, сделал тестовый запуск скрипта

--- raspberryApi/boat_hardware.py
+++ raspberryApi/boat_hardware.py
@@ -1,19 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
 
 
 class BoatHardware:
-    def __init__(self, config_path : str):
+    def __init__(self, config_path: str):
         """
         Класс, предоставляющий интерфейс для управления яхтой
         :param config_path: файл, содержащий параметры инициализации для яхты в формате .json
         """
-    def SetLed(self, led_number : int, state_flag: bool):
+        # TODO: parse hardware_configuration.json file and fill class members
+        self._ledPin = 21
+        GPIO.setup(self._ledPin, GPIO.OUT)
+
+    def SetLed(self, led_number: int, state_flag: bool):
         """
         Устанавливает габаритный светодиод led_number в положение state_flag.
         Пример: SetLed(1, True) зажигает светодиод 1.
         :param led_number: номер светодиода (0..2)
         :param state_flag: состояние светодиода, True - вкл, False - выкл
         """
-        pass
+        if state_flag:
+            GPIO.output(self._ledPin, GPIO.HIGH)
+        else:
+            GPIO.output(self._ledPin, GPIO.LOW)
 
     def SetSteeringWheel(self, grad: int):
         """
@@ -33,4 +44,11 @@ class BoatHardware:
 
 
 if __name__ == '__main__':
-    print('запустите скрпит main.py для работы с яхтой')
+    # print('запустите скрпит main.py для работы с яхтой')
+    yacht = BoatHardware("none")
+
+    while True:
+        yacht.SetLed(1, True)
+        time.sleep(1)
+        yacht.SetLed(1, False)
+        time.sleep(1)
