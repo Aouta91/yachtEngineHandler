ea68268b98a3b0a3c6b10e6296510f9b7102176f
Sergey Bachev <bachev93@mail.ru>
2020-10-22 20:40:03 +0300

    добавил парсинг json-файла с кнфигурацией периферии на борту яхты

--- raspberryApi/boat_hardware.py
+++ raspberryApi/boat_hardware.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import time
+import json
 
 GPIO.setmode(GPIO.BCM)
 
@@ -10,9 +10,20 @@ class BoatHardware:
         Класс, предоставляющий интерфейс для управления яхтой
         :param config_path: файл, содержащий параметры инициализации для яхты в формате .json
         """
-        # TODO: parse hardware_configuration.json file and fill class members
-        self._ledPin = 21
-        GPIO.setup(self._ledPin, GPIO.OUT)
+        configData = None
+        try:
+            with open(config_path) as f:
+                configData = json.load(f)
+        except FileNotFoundError:
+            configData = {'LEDs': {}}
+            configData['LEDs'].update({'nose_led': 21})
+            configData['LEDs'].update({'tail_led': 20})
+            with open('raspberryApi/hardware_configuration.json', 'w') as f_out:
+                json.dump(configData, f_out, indent=4)
+                print(f'create file {f_out.name} with default configuration')
+
+        self._noseLedPin = configData['LEDs']['nose_led']
+        GPIO.setup(self._noseLedPin, GPIO.OUT)
 
     def SetLed(self, led_number: int, state_flag: bool):
         """
@@ -21,10 +32,7 @@ class BoatHardware:
         :param led_number: номер светодиода (0..2)
         :param state_flag: состояние светодиода, True - вкл, False - выкл
         """
-        if state_flag:
-            GPIO.output(self._ledPin, GPIO.HIGH)
-        else:
-            GPIO.output(self._ledPin, GPIO.LOW)
+        GPIO.output(self._noseLedPin, state_flag)
 
     def SetSteeringWheel(self, grad: int):
         """
@@ -44,11 +52,4 @@ class BoatHardware:
 
 
 if __name__ == '__main__':
-    # print('запустите скрпит main.py для работы с яхтой')
-    yacht = BoatHardware("none")
-
-    while True:
-        yacht.SetLed(1, True)
-        time.sleep(1)
-        yacht.SetLed(1, False)
-        time.sleep(1)
+    print('запустите скрпит main.py для работы с яхтой')
